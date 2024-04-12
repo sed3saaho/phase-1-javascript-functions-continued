6b7291fafda221916b5c287805d5b75b847b5e12
Sedly <sedly.keith@student.moringaschool.com>
2024-04-12 03:27:10 +0300

    phase-1-javascript-functins-continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair='*') {
+    return function(adjective='special', visualFlair=flair) {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("%");
+console.log(encouragingPromptFunction("a dedicated programmer")); //=> "You are %a dedicated programmer%!"
